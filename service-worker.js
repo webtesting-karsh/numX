780d87c91909e5bbdef926daa41313c13deacbfe
webtesting-karsh <dezinerhappy@gmail.com>
2026-02-11 03:03:27 +0530

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = "numx-offline-v1";
+
+const ASSETS_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./script.js",
+  "./logo.png"
+];
+
+/* Install */
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+/* Activate */
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) return caches.delete(key);
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+/* Fetch */
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
